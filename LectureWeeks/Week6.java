b47f45e8f92429c4d2299a1e6229669e44342fbd
NathanLiew214 <66583135+NathanLiew214@users.noreply.github.com>
2020-12-15 04:04:57 -0600

    Add files via upload

---
+++ LectureWeeks/Week6.java
@@ -0,0 +1,33 @@
+//Monday
+//1 SQL
+
+//i)
+show databases;
+Output:information_schema, wliew
+//ii)
+use wliew;
+//iii) put in the data
+show tables;
+
+//iv) a field in the table
+describe game;
+Output: gameId,name,publisherId
+
+//v you need a primary key
+this field key is gameIfd (PRI)
+
+//vi)
+varchar is string in SQL
+
+//vii)
+describe publisher;
+Output: publisherId and name
+
+//viii)
+yellow key is a binary key
+red is a foreign key
+and blue is own key
+
+//viiii)
+
+
