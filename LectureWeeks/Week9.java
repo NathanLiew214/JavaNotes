e4ed5058fc4caece32ee7cff621be129c3d6539b
NathanLiew214 <66583135+NathanLiew214@users.noreply.github.com>
2020-12-15 04:05:31 -0600

    Update Week9.java

--- LectureWeeks/Week9.java
+++ LectureWeeks/Week9.java
@@ -28,7 +28,7 @@ public class MyLinkedList<T> {
 	public MyLinkedList() {
 		this.head = null;
 		this.size = 0;
-		this size ++;
+
 	}
 
 	public void insertAtStart(T item) {
@@ -56,7 +56,7 @@ public class MyLinkedList<T> {
 		}
 		Node <T> newEnd = new Node<T>(item);//create a Node for the item
 		curr.setNext(newEnd);//make it point to the last Node ( we had the second last as curr already)
-		
+		this.size++;
 		
 	}
 
@@ -155,20 +155,105 @@ public class MyLinkedList<T> {
 		return retrieveNodeAtIndex(index).getItem();
 	}
 
+//For Number 2
+public static void main(String[] args) {
+		MyLinkedList<Integer> One = new MyLinkedList<>();
+		One.insertAtStart(5);
+		One.insertAtStart(15);
+		One.insertAtStart(25);
+		One.insertAtStart(35);
+		One.insertAtEnd(150);
+	
+		System.out.println(One);
+		//System.out.println(One.getCount());
+		System.out.println(One.getSize());
+		System.out.println(One.toString());
+
+		One.removeFromStart();
+
+//		System.out.println(One);
+		
+		
+		System.out.println(One.retrieveItemAtIndex(0));
+		System.out.println(One.retrieveItemAtIndex(2));
+		System.out.println(One.retrieveItemAtIndex(One.size-1));
+		
+		
+		
+
+	}
+	
+	
+}
+
+35,25,15,5,150,
+4
+35,25,15,5,150,
+//remove from start become
+25,15,5,150,
+25 //index 0
+5//index 2
+150 //Retrieve last (size -1)
 
 
 
 //3 Remove At Index
+	//Remove At Index
 	public T removeAtIndex(int index) {
 		if (this.head==null) {
 			throw new IllegalArgumentException("No Element To Remove!");
 		}
-		Node <T> curr = this.retrieveNodeAtIndex(index);
-		Node <T> prev = this.retrieveNodeAtIndex(index-1);
-		prev.setNext(curr.getNext());
+		if (index==0) {
+			System.out.println("It is Empty therefore remove From Start");
+			return this.removeFromStart();
+				
+		}
 		
+	//	Node <T> curr = this.retrieveNodeAtIndex(index);
+		Node <T> prev = this.retrieveNodeAtIndex(index-1);
+		Node <T> curr = prev.getNext();
+
+		
+		prev.setNext(curr.getNext());
+		this.size -- ;
 		return curr.getItem();
 	}
+
+public static void main(String[] args) {
+		MyLinkedList<Integer> One = new MyLinkedList<>();
+		One.insertAtStart(5);
+		One.insertAtStart(15);
+		One.insertAtStart(25);
+		One.insertAtStart(35);
+		One.insertAtEnd(150);
+	
+		System.out.println(One);
+		//System.out.println(One.getCount());
+		System.out.println(One.getSize());
+		System.out.println(One.toString());
+		
+
+		System.out.println(One.removeAtIndex(0));
+	
+
+	}
+	
+	
+	
+}
+Output:
+35,25,15,5,150,
+5
+35,25,15,5,150,
+It is Empty
+35
+
+
+
+
+
+
+
 
 
 
@@ -226,7 +311,7 @@ Remove At Index
 		Node <T> curr = this.retrieveNodeAtIndex(index);
 		Node <T> prev = this.retrieveNodeAtIndex(index-1);
 		prev.setNext(curr.getNext());
-		
+		this.size -- ;
 		return curr.getItem();
 	}
 
